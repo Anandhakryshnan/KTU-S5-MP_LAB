4c309ee7b20b8f373233aa8afb6b54a32774bdb7
Anandha Krishnan <anandhakryshnan@gmail.com>
2023-12-11 20:04:51 +0530

    Update roundrobbin.c

--- Cpu_Scheduling/roundrobbin.c
+++ Cpu_Scheduling/roundrobbin.c
@@ -1,144 +1,95 @@
 #include <stdio.h>
-#include <string.h>
-int q[100],front=-1,rear=-1;
-struct process
-{
-  char name[20];
-  int at,tt,bt,wt,status,left,ct;
-}p[20],temp;
-struct done
-{
-  char name[20];
-  int st,ct;
-}d[20];
-void enqueue(int j)
-{
-  if(front==-1 && rear==-1)
-  {
-    front++;
-  }
-  rear++;
-  q[rear] = j;
+
+int i, n, quantum; /* give a time quantum */
+
+void read(int b[]) {
+    for (i = 0; i < n; ++i) {
+        printf("Enter the burst time of process %d: ", i);
+        scanf("%d", &b[i]);
+    }
 }
-int dequeue()
-{
-  int item;
-  item = q[front];
-  if(front == rear)
-  {
-    front = -1;
-    rear = -1;
-  }
-  else
-  {
-    front++;
-  }
-  return(item);
+
+void findWaitingtime(int b[], int wt[]) {
+    int b_rem[20];
+    for (i = 0; i < n; ++i) {
+        b_rem[i] = b[i];                        /* Create a copy of the burst time array */
+    }
+    int time = 0;                               /* initialize time as 0 */
+
+    while (1)
+    {
+        /* Traverse */
+
+        int flag = 0;
+
+        for (i = 0; i < n; ++i) {
+
+            if (b_rem[i] > 0) {                 /* continue only if burst time is greater than 0 */
+                flag = 1;                       /* there is a pending process */
+                if (b_rem[i] > quantum) {
+                    time += quantum;            /* shows how much time a process has been processed */
+                    b_rem[i] -= quantum;        /* Decrease the burst_time of current process by quantum */
+                }
+                else {                          /* If burst time is smaller than or equal to quantum. Last cycle for this process  */
+                    time += b_rem[i];           /* shows how much time a process has been processed */
+                    wt[i] = time - b[i];        /* Waiting time is current time minus time used by this process  */
+                    b_rem[i] = 0;               /* fully executed, so remaining burst time=0 */
+                }
+            }
+        }
+        if (flag == 0) {                        /* there is no pending process */
+            break;
+        }
+    }
 }
+
+void findTurnAroundtime(int tat[], int b[], int wt[]) {
+    for (i = 0; i < n; ++i) {
+        tat[i] = b[i] + wt[i];
+    }
+}
+
+void display(int b[], int wt[], int tat[]) {
+    int wtSum = 0, tatSum = 0;
+    printf("Process\tBurstTime WaitingTime  TurnAroundTime\n");
+    for (i = 0; i < n; ++i) {
+        wtSum += wt[i];
+        tatSum += tat[i];
+        printf("%d\t%d\t\t%d\t%d\n", i, b[i], wt[i], tat[i]);
+    }
+    printf("average waiting time: %f", (float)wtSum / n);
+    printf("\naverage turnaround time: %f", (float)tatSum / n);
+    printf("\n");
+}
+
+void calcTime(int b[], int wt[], int tat[]) {
+    findWaitingtime(b, wt);
+    findTurnAroundtime(tat, b, wt);
+    display(b, wt, tat);
+}
+
 void main()
 {
-  int n,i,j,idle=0,k,num,ls,t;
-  float avwt=0,avtt=0;
-  printf("ENTER THE NUMBER OF PROCESSES : ");
-  scanf("%d",&n);
-  for(i=0;i<n;i++)                         
-  {
-    printf("\nENTER DETAILS OF PROCESS %d",i+1);
-    printf("\nPROCESS NAME : ");
-    scanf(" %s",p[i].name);
-    printf("ARRIVAL TIME : ");
-    scanf("%d",&p[i].at);
-    printf("BURST TIME : ");
-    scanf("%d",&p[i].bt);
-    p[i].left = p[i].bt;
-    p[i].status = 0;
-  }
-  printf("\nENTER THE TIME QUANTUM : ");
-  scanf("%d",&t);
-
-  for(i=0,num=0,ls=0;ls<n;)
-  {
-    for(j=0;j<n;j++)
-    {
-      if(p[j].status==0 && p[j].at<=i)
-      {
-        enqueue(j);
-        p[j].status = 1;
-      }
-    }
-    if(idle==0 && front == -1)
-    {
-      strcpy(d[num].name,"Idle");
-      d[num].st = i;
-      idle = 1;
-      i++;
-    }
-    else if(front!=-1)
-    {
-      if(idle==1)
-      {
-        d[num].ct = i;
-        idle = 0;
-        num++;
-      }
-      k = dequeue();
-      d[num].st = i;
-      strcpy(d[num].name,p[k].name);
-      if(p[k].left<=t)
-      {
-        d[num].ct = i+p[k].left;
-        p[k].ct = d[num].ct;
-        i = d[num].ct;
-        p[k].tt = i - p[k].at;
-        p[k].wt = p[k].tt - p[k].bt;
-        p[k].status = 2;
-        ls++;
-        num++;
-      }
-      else if(p[k].left>t)
-      {
-        d[num].ct = i+t;
-        i = d[num].ct;
-        p[k].left = p[k].left-t;
-        num++;
-        for(j=0;j<n;j++)
-        {
-          if(p[j].status==0 && p[j].at<=i)
-          {
-            enqueue(j);
-            p[j].status = 1;
-          }
-        }
-        enqueue(k);
-      }
-    }
-    else
-    {
-      i++;
-    }
-  }
-
-  printf("\nPROCESS NAME\tCOMPLETION TIME (ms)\tWAITING TIME (ms)\tTURNAROUND TIME (ms)\n\n");
-  for(i=0;i<n;i++)
-  {
-    printf("    %s\t\t\t%d\t\t\t%d\t\t\t%d\n",p[i].name,p[i].ct,p[i].wt,p[i].tt);
-    avwt+=p[i].wt;
-    avtt+=p[i].tt;
-  }
-  printf("\n\nGANTT CHART ");
-  printf("\n\t--------------------------------------------------------------------------\n\t");
-  for(i=0;i<num;i++)
-  {
-    printf("|");
-    printf("%s\t",d[i].name);
-  }
-  printf(" |");
-  printf("\n\t--------------------------------------------------------------------------\n\t");
-  for(i=0;i<num;i++)
-  {
-      printf("%d\t",d[i].st);
-  }
-  printf("%d\t",d[num-1].ct);
-  printf("\n\nAVERAGE WAITING TIME : %f",(avwt/n));
-  printf("\nAVERAGE TURNAROUND TIME : %f\n",(avtt/n));
+    int b[20], tat[20], wt[20];
+    printf("Number of Processes:");
+    scanf("%d", &n);
+    read(b);
+    printf("Enter time quantum:");
+    scanf("%d", &quantum);
+    calcTime(b, wt, tat);
 }
+
+/* OUTPUT
+    Number of Processes:3
+    Enter the burst time of process 0: 54
+    Enter the burst time of process 1: 22
+    Enter the burst time of process 2: 6
+    Enter time quantum:5
+    Process BurstTime WaitingTime  TurnAroundTime
+    0       54              28      82
+    1       22              31      53
+    2       6               20      26
+    average waiting time: 26.333334
+    average turnaround time: 53.666668
+    
+*/
